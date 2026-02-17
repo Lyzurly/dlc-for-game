b00cc2b9f818b88d3c90700f8240cd7edf825037
Lýz <147095077+Lyzurly@users.noreply.github.com>
2026-02-17 03:32:17 -0600

    updates to make it more distinct

--- minigame_thinking/minigame_thinking.gd
+++ minigame_thinking/minigame_thinking.gd
@@ -19,37 +19,9 @@ var _callback_string_on_creation: Dictionary[String,Array] = {
 var _minigame_data: Dictionary[String,Dictionary] = {
 	"callback_data_on_creation": _callback_string_on_creation
 	}
-	
-#var _minigame_active: bool = false
-
-#func is_minigame_active() -> bool:
-	#return _minigame_active 
 
 func get_minigame_scene_path() -> String:
 	return _MINIGAME_SCENE_PATH
 	
 func get_minigame_data() -> Dictionary:
 	return _minigame_data
-	
-#func destroy_minigame() -> void:
-	#var minigame_instance: Node = \
-		#_minigame_node as Node
-	#minigame_instance.queue_free()
-	##_update_minigame_state(false)
-#
-#func instantiate_minigame(to_where: Node) -> void:
-	#var minigame_scene: PackedScene = \
-		#load(_MINIGAME_SCENE_PATH)
-	#var minigame_node: Node = \
-		#minigame_scene.instantiate()
-		#
-	#to_where.add_child(minigame_node)
-	
-	#_configure_instance(minigame_node)
-	#_update_minigame_state(true)
-	#
-#func _update_minigame_state(state: bool) -> void:
-	#_minigame_active = state
-
-#func _configure_instance(_instance: Node3D) -> void:
-	#pass
